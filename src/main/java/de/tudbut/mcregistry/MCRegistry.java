a714ee2e2795458f66ceba770e3b39708b75983f
TudbuT <tudbut@tudbut.de>
2023-09-17 23:32:53 +0200

    rewrite

---
+++ src/main/java/de/tudbut/mcregistry/MCRegistry.java
@@ -0,0 +1,23 @@
+package de.tudbut.mcregistry;
+
+import de.tudbut.tools.Registry;
+import java.io.IOException;
+
+public class MCRegistry {
+    public static final Registry GlobalRegistry;
+    static {
+        try {
+            GlobalRegistry = new Registry("Registry.tcnm");
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Registry registerMod(String modid) throws IllegalAccessException {
+        return new Registry(GlobalRegistry.register(modid));
+    }
+
+    public static void unregisterMod(String modid, Registry modRegistry) throws IllegalStateException, IllegalAccessException {
+        GlobalRegistry.unregister(modid, modRegistry.leak());
+    }
+}
